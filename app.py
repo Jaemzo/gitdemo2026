735f8dd1169cda5f5e85eab3624a3a8672137004
Coby Hughes <cobyhughes2008@gmail.com>
2026-01-07 12:49:20 -0500

    Update print statements in app.py

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 print ("Git/Github Demo")
 #some comment
 
-print("hello from github")
+print("hello from github 2")
 
 
-print("hello from VS Code")
+print("hello from VS Code 3")
+
+print("Hey local you aint so tough")
